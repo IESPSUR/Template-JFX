e3e2c9f0b78c89c3954d6c1739e7c0e95439d7e5
Tu Nombre <Eduardo Serrano Luque>
2021-03-18 11:34:57 +0100

    template acabado

---
+++ src/main/java/gui/viewsandcontrollers/main/MainController.java
@@ -0,0 +1,33 @@
+package gui.viewsandcontrollers.main;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class MainController {
+	
+	@FXML
+    private Label label;
+	
+	@FXML
+    private TextField textfield;
+
+    public void initialize() {
+        // TODO
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException {
+            
+       
+    }
+}
